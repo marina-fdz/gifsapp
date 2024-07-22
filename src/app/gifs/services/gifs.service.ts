ff13d0c9c8afe7e9bfdef32704cb3b894dbd3a18
marina-fdz <marina.fdz.caceres@gmail.com>
2024-07-22 19:09:50 +0200

    display fetched data

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import {  HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+
+  public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
   private apiKey:     string = 'iJC2j9gN1Mcxkhj7DVWvJmY74wBNeCuw';
@@ -34,9 +37,11 @@ export class GifsService {
     .set('q', tag)
 
 
-    this.http.get(`${this.serviceUrl}/search`, { params })
-    .subscribe( resp => {
-      console.log(resp);
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+    .subscribe( (resp) => {
+
+      this.gifList = resp.data;
+
     })
 
   }
