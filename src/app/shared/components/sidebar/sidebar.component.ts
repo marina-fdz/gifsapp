77a78fed7308336da36a4cc3498159aebab60643
marina-fdz <marina.fdz.caceres@gmail.com>
2024-07-22 20:04:25 +0200

    add buttons

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
@@ -10,8 +10,12 @@ export class SidebarComponent {
 
   constructor( private gifsService: GifsService ) { }
 
-  get tags() {
+  get tags(): string[] {
     return this.gifsService.tagsHistory;
+  }
+
+  searchTag( tag: string ){
+    this.gifsService.searchTag(tag);
   }
 
 }
