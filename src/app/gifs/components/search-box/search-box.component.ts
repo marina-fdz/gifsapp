660b93c3b5a9136a4adf080dded9a6ba59594df4
marina-fdz <marina.fdz.caceres@gmail.com>
2024-07-22 12:10:35 +0200

    add viewchild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
   <h5>Search:</h5>
-  <input type="text" class="form-control" name="" id="" placeholder="Search gifs...">
+  <input type="text" class="form-control" name="" id="" placeholder="Search gifs..." (keyup.enter)="searchTag()" #txtTagInput>
   `
 })
 
 export class SearchBoxComponent  {
-  constructor() { }
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor() {}
+
+  searchTag( ){
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 }
